6547da6a5e85d546cce73e5cc3ebf7d09afc9bc2
Jack <jack@toumey.io>
2019-05-19 12:58:51 -0700

    moving favicon

--- gin-web/main.go
+++ gin-web/main.go
@@ -12,7 +12,7 @@ func main() {
 
 	// configuration for static files and templates
 	engine.LoadHTMLGlob("./templates/*.html")
-	engine.StaticFile("/favicon.ico", "./favicon.ico")
+	engine.StaticFile("/favicon.ico", "./assets/favicon.ico")
 
 	engine.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", gin.H{
